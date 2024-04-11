2eed2221c6651e5d6e7b5b03c1773e9ccb94768c
start.vaadin.com <no-reply@vaadin.com>
2024-04-11 06:36:37 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/deadlock/DeadlockView.java
@@ -0,0 +1,35 @@
+package com.example.application.views.deadlock;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Deadlock")
+@Route(value = "deadlock", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class DeadlockView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public DeadlockView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
